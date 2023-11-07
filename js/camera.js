2a8a8b75c0bcda6c61d40dc4391f2b82ffa05292
翊誠 呂 <HOT60310@gmail.com>
2023-11-07 17:35:02 +0800

    transferETH & transferCC & ConvertCC 相機可自動填result

--- js/camera.js
+++ js/camera.js
@@ -1,16 +1,11 @@
-//獲取按鈕元素
-const startCameraTransETHButton = document.getElementById('startCameraTransETH');
-//獲取按鈕元素(未改動 transferCC convertCC) 利用判斷網址來判斷用哪個函式
-
-
-const startCameraTransCCButton = document.getElementById('startCameraTransCC');
-//const startCameraButton2 = document.getElementById('startCamera2');
+//獲取Button元素
+const startCameraButton = document.getElementById('startCamera');
 
 //獲取video元素
 const cameraView = document.getElementById('cameraView');
 
 //在按鈕點擊時請求相機權限(transferETH)
-startCameraTransETHButton.addEventListener('click', async () => {
+startCameraButton.addEventListener('click', async () => {
     try {
         //請求用戶相機
         const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -29,17 +24,66 @@ startCameraTransETHButton.addEventListener('click', async () => {
                 const codeArr = code.data.split(',');
                 // 取出前2個元素 
                 const [Acc, AccNum] = codeArr.slice(0, 2);
+                // 取出前2個元素
+                const [Transportation, TransKM] = codeArr.slice(0, 2);
                 const element = document.getElementById('QRresult');
-
-                if (element) {
-                    //找到要填寫的<input>
-                    const AccInput = document.getElementById('TransAcc');
-                    const AccNumInput = document.getElementById('TransAccNum');
+                //通過location對象的href屬性獲取當前頁面的URL
+                const currentUrl = location.href;
+                const pageURL = document.getElementById('pageURL');
+                //若是transferETH就把QRcode內容填入對應表格
+                if (currentUrl.indexOf('transferETH') > -1) {
+                    pageURL.textContent = 'Current page: ' + currentUrl;
+                    if (element) {
+                        //找到要填寫的<input>
+                        const AccInput = document.getElementById('TransAcc');
+                        const AccNumInput = document.getElementById('TransAccNum');
+                        //填寫結果
+                        AccInput.value = Acc;
+                        AccNumInput.value = AccNum;
+                        //顯示結果到網頁
+                        element.textContent = 'Acc: ' + Acc + ' AccNum: ' + AccNum;
+                    }
+                } else if (currentUrl.includes('transferCC')) {
+                    pageURL.textContent = 'Current page: ' + currentUrl;
+                    if (element) {
+                        //找到要填寫的<input>
+                        const AccInput = document.getElementById('CCTransAcc');
+                        const AccNumInput = document.getElementById('CCTransAccNum');
+                        //填寫結果
+                        AccInput.value = Acc;
+                        AccNumInput.value = AccNum;
+                        //顯示結果到網頁
+                        element.textContent = 'Acc: ' + Acc + ' AccNum: ' + AccNum;
+                    }
+                } else if (currentUrl.includes('ConvertCC')) {
+                    pageURL.textContent = 'Current page: ' + currentUrl;
+                    //找到要填寫的<select>
+                    const TransSelect = document.getElementById('Transportation');
+                    const TransKMSelect = document.getElementById('TransKM');
                     //填寫結果
-                    AccInput.value = Acc;
-                    AccNumInput.value = AccNum;
+                    TransSelect.value = Transportation;
+                    TransKMSelect.value = TransKM;
+                    Trans = ''
+                    switch (Transportation) {
+                        case '0':
+                            Trans = 'bicycle'
+                            break;
+                        case '1':
+                            Trans = 'bus'
+                            break;
+                        case '2':
+                            Trans = 'train'
+                            break;
+                        case '3':
+                            Trans = 'motorcycle'
+                            break;
+                        case '4':
+                            Trans = 'car'
+                            break;
+                        default:
+                    }
                     //顯示結果到網頁
-                    element.textContent = 'Acc: ' + Acc + ' AccNum: ' + AccNum; 
+                    element.textContent = 'Transportation: ' + Trans + ' TransKM: ' + TransKM;
                 }
 
             }, 1000);
